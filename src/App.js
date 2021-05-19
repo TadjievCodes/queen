849de9fabda77879c63511932ac214f9a67eff35
TadjievCodes <muhammad2008.08@mail.ru>
2021-05-19 03:33:14 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import FormW from './components/Form';
+import { Container, Button } from 'react-bootstrap'
+import promo from './assets/1.jpg'
+import { FaFacebook, FaYoutube, FaInstagram } from 'react-icons/fa'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <h1 id='h1' className='text-center m-3'>THE HOT CASTOR OIL HAIR MASK RECIPE FOR HAIR GROWTH</h1>
+      <p id='bonus' className='text-center m-3'>+ BONUS Essential Oil Recipe!</p>
+
+      <div id='hero' className='d-flex flex-md-row flex-sm-column align-items-center'>
+
+        <div className= 'col-md-7 col-sm-12 col-xs-12'><img className='img-fluid' src={promo} alt='promo'/></div>
+        <FormW />
+
+      </div>
+        <div className='d-flex justify-content-center align-items-center m-5'>
+          <div id='line'></div>
+          <h2 className=' text-dark col-4 text-center'>Why Castor Oil?</h2>
+          <div id='line'></div>
+        </div>
+
+      <div id='oil' className='d-flex flex-md-row flex-sm-column align-items-center'>
+
+        <div className='oil-text col-md-6 col-sm-12 col-xs-12'>
+          <p>Touted as the best oil to <b>stimulate hair growth</b> since Ancient Egyptian times, castor oil was the legendary beauty secret of <b>Queen Cleopatra</b> herself. </p>
+          <p>Castor oil on its own nourishes, improves circulation and reduces inflammation on the hair follicle helping to <b>reduce hair loss, relieve itchy scalp </b> and <b> grow your hair thick and strong.  </b> </p>
+          <p>It's also the very best carrier oil! This is why you should always ensure your castor oil is:</p>
+          <ul>
+            <li>Highest quality &amp; 100% pure</li>
+            <li>Certified organic </li>
+            <li>Cold-pressed, hexane-free  extra-virgin</li>
+            <li>Always bottled in glass</li>
+          </ul>
+          <p>Castor oil helps cells absorb, so you can upgrade your hair mask with the optional <b> 7 secret synergistic essential oils for hormones & hair </b> included in this recipe. This clinically practiced, patient-preferred, & scientifically supported method helps to drive nutrients deep into hair follicles – even for hair loss caused by <b> hormonal problems, alopecia, thyroid issues, Hashimoto’s, menopause, and more. </b></p>
+        </div>
+
+        <div  className='oil-text col-md-6 col-sm-12 col-xs-12'><img className='img-fluid' src='https://cdn05.zipify.com/G--KdKEwiGyfuGavjxMjPcCRf4Y=/fit-in/1080x0/44f66d04014d4502863c1fa63ad7b463/cosmetic.png' alt='promo'/></div>
+
+      </div>
+      <img className='img-fluid' src='https://cdn05.zipify.com/aVGhiDMaiNqImPpqveiY2dy7-RE=/fit-in/2048x0/895d96f24eaf4dcab5310cc544a05cd1/dream-hair-benefit-bullets.jpg' />
+      <h2 className='text-center mt-5'>ENTER YOUR EMAIL NOW!</h2>
+      <p className='text-center'>To Unlock This Free, Less-mess, Simple Self-care Recipe For Natural Hair Growth.</p>
+      <div className='text-center m-3'>
+       <Button variant='success'  className='p-4'> Get it NOW!</Button>
+      </div>
+      <div className='text-center m-3'>
+        <img className='img-fluid' width='150px' src='https://cdn05.zipify.com/Q1AEnhd3irxyGsRdtLKhSZVWiKY=/fit-in/2048x0/4afac8dd831844d992f2a3431ac40819/6ab2bd-full.png'  />
+      </div>
+      <p className='text-center'>For all general inquiries, please contact us at <a href='mailto:care@drmarisol.com'>care@drmarisol.com</a></p>
+      <div className='text-center m-3 mt-5'>
+        <FaFacebook />
+        <FaYoutube />
+        <FaInstagram />
+      </div>
+
+      <p className='text-center mt-5'>©2020 Copyright. All rights reserved</p>
+  </Container>
   );
 }
 
---
+++ src/components/Form.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap'
+import pdf from '../assets/1.pdf'
+
+function FormW() {
+
+  const [fname, setFname] = useState('')
+  const [email, setEmail] = useState('')
+  const [show, setShow] = useState(false);
+
+  const nameHandler = (e) => {
+    setFname(e.target.value);
+  }
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const submitBtn = (e) => {
+    e.preventDefault();
+
+    // validation
+
+    if ((fname && email) == '') {
+      setShow(true)
+    } else {
+      window.open(pdf)
+    }
+
+  }
+
+
+
+  return (
+    <div className=' col-md-5 col-sm-12 col-xs-12 m-5 p-2'>
+    
+      <Form>
+        <h3>Just Tell Us Where To Send Your FREE Castor Oil Hair Growth Recipe! (+ Bonus Essential Oil Recipe)</h3>
+
+        <Form.Group controlId="formBasicName">
+          <Form.Control
+            size="lg"  
+            type="fname" 
+            placeholder="First Name" 
+            onInput={nameHandler} 
+            value={fname} 
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Control 
+            size="lg"
+            type="email"
+            placeholder="Email Address" 
+            onInput={emailHandler} 
+            value={email} 
+  
+             
+            />
+        </Form.Group>
+
+        <Button variant='success'  className='p-4 w-100' onClick={submitBtn} > Get it NOW!</Button>
+
+      </Form>
+    <Alert show={show} className='mt-3' variant="danger" onClose={() => setShow(false)} dismissible>Please enter name and email</Alert>
+    </div>
+  );
+}
+
+export default FormW;
